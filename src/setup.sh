147b0b525db52394105763428dd03c6aac5d66a4
chinmaynehate <chinmaynehate@gmail.com>
2020-08-21 20:22:08 +0530

    Added setup.sh

---
+++ src/setup.sh
@@ -0,0 +1,6 @@
+pip3 install tensorflow==1.15
+sudo apt-get install -y python3-pyaudio
+pip3 install keras==2.3.1
+pip3 install torch==1.5.0
+pip3 install pydub==0.24.1
+pip3 install torchvision==0.6.0
