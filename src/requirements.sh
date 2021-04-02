9b8376b2d4c970724121195d18d09768ecea7022
chinmaynehate <chinmaynehate@gmail.com>
2021-04-02 18:56:04 +0530

    Updated requirements

---
+++ src/requirements.sh
@@ -0,0 +1,15 @@
+pip3 install --upgrade pip
+python3 -m pip3 install --upgrade setuptools
+sudo apt install libasound2-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
+pip3 install librosa
+pip3 install matplotlib
+pip3 install h5py==2.10.0
+pip3 install tensorflow==1.15
+sudo apt-get install -y python3-pyaudio
+pip3 install keras==2.3.1
+pip3 install torch==1.5.0
+pip3 install pydub==0.24.1
+pip3 install torchvision==0.6.0
+chmod +x pyaudio.sh
+./pyaudio.sh
+
